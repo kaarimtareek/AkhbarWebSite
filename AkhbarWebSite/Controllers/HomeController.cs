c5713766dd2449d23baaf9bc4068d20774ce2980
kaarimtareek <kaarimtareek@gmail.com>
2020-08-04 17:27:06 +0200

    add contact form

--- AkhbarWebSite/Controllers/HomeController.cs
+++ AkhbarWebSite/Controllers/HomeController.cs
@@ -53,6 +53,10 @@ namespace AkhbarWebSite.Controllers
         {
             return View();
         }
+        public IActionResult Contact()
+        {
+            return View();
+        }
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
